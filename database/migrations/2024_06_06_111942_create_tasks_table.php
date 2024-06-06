85782284e22173a2448afd3cc21579d51cbdcb55
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-06-06 16:21:27 +0100

    Create User, Task Model && migrations

---
+++ database/migrations/2024_06_06_111942_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('titre');
+            $table->text('description');
+            $table->enum('statut',['Waiting','Processing','Completed'])->default('Waiting');
+            $table->date('due_date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
