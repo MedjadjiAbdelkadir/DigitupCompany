03efd528b16fbeee3641b148435a68acf2bbb96e
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-06-06 16:28:16 +0100

    Update

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Task\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +17,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json($request->user());
 });
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('logout', [AuthController::class, 'logout']);
+    // Route::get('user', [AuthController::class, '']);
+
+    Route::get('tasks', [TaskController::class, 'index']);
+    Route::post('tasks', [TaskController::class, 'store']);
+    Route::post('tasks/{id}', [TaskController::class, 'show']);
+    Route::put('tasks/{id}', [TaskController::class, 'update']);
+    Route::delete('tasks/{id}', [TaskController::class, 'destroy']);
+    Route::get('tasks/deleted', [TaskController::class, 'restore'])->middleware('role:admin');
+
+    // restore
+});
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
